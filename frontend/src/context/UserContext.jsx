fd9636ce153c998dfa43727d5503cef2e638d1d4
pooja <pgiri37484@gmail.com>
2026-04-08 22:33:08 +0530

    fix cors issue

--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -3,7 +3,7 @@ import { createContext, useEffect, useState } from "react"
 
 export const userDataContext=createContext()
 function UserContext({children}){
-    const serverUrl="https://assistant-backend-rnun.onrender.com"
+    const serverUrl="https://your-backend.vercel.app/api"
     const [userData,setUserData]=useState(null)
         const [frontendImage, setFrontendImage] = useState(null)
         const [backendImage, setBackendImage] = useState(null)
