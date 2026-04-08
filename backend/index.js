87a338d5ab095bc64a5820445919e12f3107ac4b
pooja <pgiri37484@gmail.com>
2026-04-08 23:10:10 +0530

    fix cors crash

--- backend/index.js
+++ backend/index.js
@@ -7,11 +7,16 @@ import authRouter from './routes/auth.routes.js'
 import userRouter from './routes/user.routes.js'
 dotenv.config()
 
-const app=express()
+const app = express();
+
 app.use(cors({
-  origin:"http://localhost:5173",
-  credentials:true
-}))
+  origin: [
+    "http://localhost:5173",
+    "https://assistant-frontend-lc3f.onrender.com"
+  ],
+  credentials: true
+}));
+
 const port=process.env.PORT || 5000
 app.use(express.json())
 app.use(cookieParser())
