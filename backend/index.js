1029937978898f9ed1c8bb4bcf7d2d9e7ab94efe
pooja <pgiri37484@gmail.com>
2026-04-09 00:09:26 +0530

    Your commit message

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ dotenv.config()
 
 const app=express()
 app.use(cors({
-  origin: ["http://localhost:5173/", "https://assistant-frontend-lc3f.onrender.com"],
+  origin: ["http://localhost:5173/", "https://your-frontend.vercel.app/"],
   credentials: true
 }));
 const port=process.env.PORT || 5000
